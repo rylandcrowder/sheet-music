2d99ba92df8683b374278469917df6c07b40e680
datadragonsfll <87546195+datadragonsfll@users.noreply.github.com>
2021-07-30 15:52:35 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,23 @@
+function good_king_wensaloss () {
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    pause(30)
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    pause(30)
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    pause(30)
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Double))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(349, music.beat(BeatFraction.Double))
+    pause(30)
+    music.playTone(349, music.beat(BeatFraction.Double))
+    pause(1000)
+}
+forever(function () {
+    good_king_wensaloss()
+})
