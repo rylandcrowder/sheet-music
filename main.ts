5965fe00864594ce9bf391aff8d04d0a5bb2717f
rylandcrowder <88215803+rylandcrowder@users.noreply.github.com>
2021-08-09 07:07:57 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,27 @@
-function good_king_wensaloss () {
-    music.playTone(349, music.beat(BeatFraction.Whole))
-    pause(30)
-    music.playTone(349, music.beat(BeatFraction.Whole))
-    pause(30)
-    music.playTone(349, music.beat(BeatFraction.Whole))
-    music.playTone(392, music.beat(BeatFraction.Whole))
-    music.playTone(349, music.beat(BeatFraction.Whole))
-    pause(30)
-    music.playTone(349, music.beat(BeatFraction.Whole))
-    music.playTone(262, music.beat(BeatFraction.Double))
-    music.playTone(294, music.beat(BeatFraction.Whole))
-    music.playTone(262, music.beat(BeatFraction.Whole))
-    music.playTone(294, music.beat(BeatFraction.Whole))
-    music.playTone(330, music.beat(BeatFraction.Whole))
-    music.playTone(349, music.beat(BeatFraction.Double))
-    pause(30)
-    music.playTone(349, music.beat(BeatFraction.Double))
-    pause(1000)
+function Good_King_whensaloss (onoff: boolean, Repeat: number) {
+    if (onoff) {
+        for (let index = 0; index < Repeat; index++) {
+            music.playTone(349, music.beat(BeatFraction.Whole))
+            pause(30)
+            music.playTone(349, music.beat(BeatFraction.Whole))
+            pause(30)
+            music.playTone(349, music.beat(BeatFraction.Whole))
+            music.playTone(392, music.beat(BeatFraction.Whole))
+            music.playTone(349, music.beat(BeatFraction.Whole))
+            pause(30)
+            music.playTone(349, music.beat(BeatFraction.Whole))
+            music.playTone(262, music.beat(BeatFraction.Double))
+            music.playTone(294, music.beat(BeatFraction.Whole))
+            music.playTone(262, music.beat(BeatFraction.Whole))
+            music.playTone(294, music.beat(BeatFraction.Whole))
+            music.playTone(330, music.beat(BeatFraction.Whole))
+            music.playTone(349, music.beat(BeatFraction.Double))
+            pause(30)
+            music.playTone(349, music.beat(BeatFraction.Double))
+            pause(1000)
+        }
+    }
 }
 forever(function () {
-    good_king_wensaloss()
+    Good_King_whensaloss(true, 1)
 })
